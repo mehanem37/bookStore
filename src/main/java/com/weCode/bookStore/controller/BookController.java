517a0845a28c07b69094e00083ac50b989a50e1c
Menahem <ordres.vs@gmail.com>
2021-07-26 12:51:01 +0300

    jdk change

--- src/main/java/com/weCode/bookStore/controller/BookController.java
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/vi/books")
+@RequestMapping("api/vi/bookStore")
 public class BookController {
 
     @GetMapping
